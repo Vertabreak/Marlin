1002fa9984231ba8eca68f20e37c9d77521529ca
Vertabreaker <opyrus@hotmail.com>
2020-01-05 19:32:05 -0500

    adjust macros to 10

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2645,8 +2645,8 @@
  */
 #define GCODE_MACROS
 #if ENABLED(GCODE_MACROS)
-  #define GCODE_MACROS_SLOTS       5  // Up to 10 may be used
-  #define GCODE_MACROS_SLOT_SIZE  50  // Maximum length of a single macro
+  #define GCODE_MACROS_SLOTS      10  // Up to 10 may be used
+  #define GCODE_MACROS_SLOT_SIZE  25  // Maximum length of a single macro
 #endif
 
 /**
