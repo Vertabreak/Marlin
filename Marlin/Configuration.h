58ae698ee587dc0a9807277651fa4deee52f69b9
Potterer <jewallin@me.com>
2021-10-02 15:11:28 +0200

    PIDtuned

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -606,9 +606,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  22.20
-    #define DEFAULT_Ki   1.08
-    #define DEFAULT_Kd 114.00
+    #define DEFAULT_Kp  30.90
+    #define DEFAULT_Ki   3.45
+    #define DEFAULT_Kd  69.20
   #endif
 #endif // PIDTEMP
 
