5071bfc569207bda7b94db310adebc874a36b612
Vert <Vert@vert.com>
2023-11-20 16:18:42 -0500

    base

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -70,7 +70,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_BTT_SKR_V3_0
+  #define MOTHERBOARD BOARD_BTT_MANTA_M5P_V1_0
 #endif
 
 /**
